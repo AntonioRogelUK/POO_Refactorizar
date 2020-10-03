1f9310c15f7c6a1b4a8b0c43ec1fe193010a72a9
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2020-10-03 13:38:14 -0700

    Refactorizado

---
+++ POO_Refactorizado/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Refactorizado
+{
+    public class Persona
+    {
+        private string nombre;
+        private DateTime fechaNacimiento;
+
+        public string Nombre { get { return nombre; } }
+        public DateTime FechaNacimiento { get { return fechaNacimiento; } }
+        public int Edad { get { return CalcularEdad(); } }
+
+        public Persona(string nombre, DateTime fechaNacimiento)
+        {
+            if (nombre.Length > 1)
+            {
+                this.nombre = $"{nombre.Substring(0, 1).ToUpper()}{nombre.Substring(1).ToLower()}";
+            }
+            else
+            {
+                this.nombre = nombre.ToUpper();
+            }
+
+            this.fechaNacimiento = fechaNacimiento;
+        }
+
+        private int CalcularEdad()
+        {
+            int EdadPersona = DateTime.Now.Year - fechaNacimiento.Year;
+            if (DateTime.Now.Month == fechaNacimiento.Month)
+            {
+                if (DateTime.Now.Day < fechaNacimiento.Day)
+                {
+                    EdadPersona--;
+                }
+            }
+            else if (DateTime.Now.Month < fechaNacimiento.Month)
+            {
+                EdadPersona--;
+            }
+            return EdadPersona;
+        }
+
+        public void Imprime()
+        {
+            Console.WriteLine($"{Nombre} tiene {Edad} años, su fecha de nacimiento es {fechaNacimiento.ToString("dd/MM/yyyy")}");
+        }
+    }
+}
---
+++ POO_Refactorizado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace POO_Refactorizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("juan", new DateTime(1980, 10, 19));
+            Persona persona2 = new Persona("manuel", new DateTime(1983, 12, 22));
+            Persona persona3 = new Persona("abraham", new DateTime(1992, 11, 01));
+            Persona persona4 = new Persona("ramon", new DateTime(1988, 09, 15));
+            Persona persona5 = new Persona("ricardo", new DateTime(1974, 04, 8));
+            Persona persona6 = new Persona("hugo", new DateTime(1980, 4, 3));
+            Persona persona7 = new Persona("jose", new DateTime(1983, 2, 1));
+            Persona persona8 = new Persona("MIGUEL", new DateTime(1980, 2, 1));
+
+            persona1.Imprime();
+            persona2.Imprime();
+            persona3.Imprime();
+            persona4.Imprime();
+            persona5.Imprime();
+            persona6.Imprime();
+            persona7.Imprime();
+            persona8.Imprime();
+        }
+    }
+}
