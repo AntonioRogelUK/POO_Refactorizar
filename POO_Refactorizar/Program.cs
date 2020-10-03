a4f6e4fe2928d880d40673443ac9cf231b8f5c5e
AntonioRogelUK <63703411+AntonioRogelUK@users.noreply.github.com>
2020-10-03 13:00:49 -0700

    Agregar archivos de proyecto.

---
+++ POO_Refactorizar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace POO_Refactorizar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
